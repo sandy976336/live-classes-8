63bbafc2b706e05ff3f6dee6db2402da48fe7fee
Akas Patranobis <akaspatranobis@yahoo.com>
2025-05-03 11:41:30 +0530

    up

---
+++ terraform/conditions/main.tf
@@ -0,0 +1,16 @@
+# ? and :
+# condition ? true_value : false_value
+
+
+variable "environment" {
+  default = "dev"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0533f2ba8a1995cf9"
+  instance_type = var.environment == "prod" ? "t3.medium" : "t3.micro"
+
+  tags = {
+    Name = "EC2 - ${var.environment}"
+  }
+}
